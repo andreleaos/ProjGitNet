26a51896c67c6fe7c27a096e9c7bb6d4c052a311
andre <andre@ANDRE-PC>
2018-05-05 22:53:00 -0300

    Add project files.

---
+++ ProjGitNet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjGitNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto Git Criado com Sucesso!");
+        }
+    }
+}
